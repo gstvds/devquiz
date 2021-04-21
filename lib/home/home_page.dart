b0cdc3690297d32c34a17f2539c9ef1fe3b71e76
Gustavo Silva <gstvds@icloud.com>
2021-04-21 19:08:20 -0300

    feat: Adds LevelButton widget

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/home/widgets/appbar/appbar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBarWidget());
+    return Scaffold(
+      appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Fácil",
+            ),
+            LevelButtonWidget(
+              label: "Médio",
+            ),
+            LevelButtonWidget(
+              label: "Difícil",
+            ),
+            LevelButtonWidget(
+              label: "Perito",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
