8fa0e2a99077a60780a4a2d2fe187573ad3913d4
Gustavo Silva <gstvds@icloud.com>
2021-04-25 19:50:57 -0300

    feat: Adds NextButton widget

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,20 @@ class _ChallengePageState extends State<ChallengePage> {
         child: SafeArea(top: true, child: QuestionIndicator()),
       ),
       body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.secondary("Pular")),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.primary("Confirmar"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
